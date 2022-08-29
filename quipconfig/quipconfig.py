b79cfcae380d7eb1ba52d9c73a830328588cb38a
Bryan J Myers <myersb89@gmail.com>
2022-08-29 16:07:47 -0500

    read in a basic role config

--- quipconfig/quipconfig.py
+++ quipconfig/quipconfig.py
@@ -1,6 +1,18 @@
+import argparse
+import yaml
+import pathlib, os
 
 def main():
-    print("Hello World")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("role", nargs='?', type=str, default="web", help="The role for the server. Determines which configuration to apply")
+    args = parser.parse_args()
+    role = args.role
 
-if __name__ == "__main__":
-    main()
+    print(f"Configuring server of type: {role}")
+
+    path = pathlib.Path(pathlib.Path(__file__).resolve().parent / f"configs\{role}_config.yml")
+    with open(path, 'r') as config_file:
+        data = config_file.read()
+        content = yaml.safe_load(data)
+        print(content)
+        
