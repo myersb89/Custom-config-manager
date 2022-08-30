1c7ada8fe50f1882df11bad981fe761ffa3a9d3d
Bryan J Myers <myersb89@gmail.com>
2022-08-29 19:11:30 -0500

    refactor and todos

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Custom-config-manger
+TODOS:
+- Add testing framework
+- Errors and edge cases
+- Decide on push or pull architecture
+    push:
+    - look into remote code execution with python
+    - threading/parallelization?
+    pull:
+    - how to get it installed
+    - should it actually connect and download config, or run a local copy?
+- Document how to create a config
+    - bonus, add a wizard?
--- quipconfig/quipconfig.py
+++ quipconfig/quipconfig.py
@@ -4,6 +4,8 @@ import pathlib, os
 from .quipConfigFile import QuipConfigFile
 from .quipConfigPackage import QuipConfigPackage
 
+from typing import Tuple
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("role", nargs='?', type=str, default="web", help="The role for the server. Determines which configuration to apply")
@@ -16,12 +18,16 @@ def main():
     print(f"Configuring server of type: {role}")
 
     # Read and Parse the Configuration
+    files, packages = read_role_config(role)
+
+    print(files)
+    print(packages)
+
+def read_role_config(role: str) -> Tuple[list[QuipConfigFile], list[QuipConfigPackage]]:
     path = pathlib.Path(pathlib.Path(__file__).resolve().parent / f"configs\{role}_config.yml")
     with open(path, 'r') as config_file:
         data = config_file.read()
         content = yaml.safe_load(data)
         files = content.get("files")
         packages = content.get("packages")
-
-    print(files)
-    print(packages)
+        return content.get("files"), content.get("packages")
